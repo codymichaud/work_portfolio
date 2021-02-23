97db8e0b15ed13c2b556f4a50aac8af0b23d4d37
Cody Michaud <cody.r.michaud@gmail.com>
2021-02-23 12:30:07 -0800

    added scrollbar. changed nav content. added links for future pages

---
+++ assets/js/portfolio.js
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-custom">
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a id="font" class="nav-link" href="./index.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a id="font" class="nav-link" href="./about_me.html">About Me</a>
+            </li>
+            <li class="navitem">
+                <a id="font" class="nav-link" href="./menu.html">Projects</a>
+            </li>
+            <li class="nav-item">
+                <a id="font" class="nav-link" href="./contact.html">Contact Me!</a>
+            </li>
+
+
+        </ul>
+    </div>
+
+    <div class="mx-auto titleLatteDaa container-fluid font name" style="width: 216px;">
+        Cody Michaud
+        </div>
